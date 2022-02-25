9aa82dffd1d027427387ba5f23292b5e333657ae
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-24 20:15:37 -0500

    Pair programming task with @dazycki

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,42 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['B', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['D', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['O', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['N', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['E', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'ABANDONEE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'ABANDONEE')
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (backwards)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  // Put your solution here
+  let arr = [];
+  //This loop creates the amount of arrays  equal to the total number of "x" values, from the original array
+  for (let k = 0; k < matrix[0].length; k++) {
+    arr.push([]);
+  }
+  //Loop through the "y" values - outer array
+  for (let y = 0; y < matrix.length; y++) {
+    //Loop through the "x" values - inner array values
+    for (let x = 0; x < matrix[y].length; x++) {
+      //inverts the "x" and "y" values
+      arr[x][y] = matrix[y][x];
     }
-}
+  }
+  return arr;
+};
 
-module.exports = wordSearch
+let reverse = function(word) {
+  return word.split("").reverse().join("");
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const backwardsHorizontal = [];
+  for (x of horizontalJoin) {
+    backwardsHorizontal.push(reverse(x));
+  }
+  const verticalArray = transpose(letters);
+  const verticalJoin = verticalArray.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of backwardsHorizontal) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
